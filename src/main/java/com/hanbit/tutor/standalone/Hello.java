4c02ad6d624d6c3ede899faebdf3301d0217ca39
hb <hb@hb5000>
2016-09-09 12:32:07 +0900

    코딩 관례 설명

--- src/main/java/com/hanbit/tutor/standalone/Hello.java
+++ src/main/java/com/hanbit/tutor/standalone/Hello.java
@@ -2,9 +2,21 @@ package com.hanbit.tutor.standalone;
 
 public class Hello {
 
+	public int studentName;
+
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("How are you?");
+	}
+
+	public void studyJava() {
+
+	}
+
+	public void goHome() {
+		if (true) {
+
+		}
 	}
 
 }
