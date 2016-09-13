1faaab0515164cb9b124e3c7daca2a3b13f3711e
hb <hb@hb5000>
2016-09-13 10:30:02 +0900

    DAO 추가 및 공통화

---
+++ src/main/java/com/hanbit/tutor/core/dao/ScheduleDAO.java
@@ -0,0 +1,100 @@
+package com.hanbit.tutor.core.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hanbit.tutor.core.vo.ScheduleVO;
+
+public class ScheduleDAO {
+
+	private Connection getConnection() {
+		String url = "jdbc:oracle:thin:@127.0.0.1/xe";
+		String user = "hanbit";
+		String password = "hanbit";
+
+		Connection connection = null;
+
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			connection = DriverManager.getConnection(url, user, password);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	private void closeConnection(Connection connection) {
+		try {
+			connection.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private int executeSql(Connection connection, String sql, List params) {
+		int result = 0;
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			for (int i=0;i<params.size();i++) {
+				statement.setObject(i + 1, params.get(i));
+			}
+
+			result = statement.executeUpdate();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public int insertSchedule(ScheduleVO schedule) {
+		Connection connection = getConnection();
+
+		String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) "
+				+ " VALUES(?, ?, ?, ?, ?)";
+
+		List params = new ArrayList();
+		params.add(schedule.getScheduleId());
+		params.add(schedule.getTitle());
+		params.add(schedule.getMemo());
+		params.add(schedule.getStartDt());
+		params.add(schedule.getEndDt());
+
+		int result = executeSql(connection, sql, params);
+
+		closeConnection(connection);
+
+		return result;
+	}
+
+	public int updateSchedule(ScheduleVO schedule) {
+		Connection connection = getConnection();
+
+		String sql = "UPDATE SCHEDULE SET TITLE = ?, MEMO = ?, "
+				+ "START_DT = ?, END_DT = ? "
+				+ "WHERE SCHEDULE_ID = ?";
+
+		List params = new ArrayList();
+		params.add(schedule.getTitle());
+		params.add(schedule.getMemo());
+		params.add(schedule.getStartDt());
+		params.add(schedule.getEndDt());
+		params.add(schedule.getScheduleId());
+
+		int result = executeSql(connection, sql, params);
+
+		closeConnection(connection);
+
+		return result;
+	}
+
+}
--- src/main/java/com/hanbit/tutor/core/service/SchedulerService.java
+++ src/main/java/com/hanbit/tutor/core/service/SchedulerService.java
@@ -1,56 +1,16 @@
 package com.hanbit.tutor.core.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
+import com.hanbit.tutor.core.dao.ScheduleDAO;
 import com.hanbit.tutor.core.vo.ScheduleVO;
 
 public class SchedulerService {
 
 	public int addSchedule(ScheduleVO schedule) {
-		String url = "jdbc:oracle:thin:@127.0.0.1/xe";
-		String user = "hanbit";
-		String password = "hanbit";
+		ScheduleDAO scheduleDAO = new ScheduleDAO();
 
-		/*String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) "
-				+ " VALUES('" + schedule.getScheduleId() + "', "
-				+ "'" + schedule.getTitle() + "', "
-				+ "'" + schedule.getMemo() + "', "
-				+ "'" + schedule.getStartDt() + "', "
-				+ "'" + schedule.getEndDt() + "')";*/
-
-		String sql = "INSERT INTO SCHEDULE (SCHEDULE_ID, TITLE, MEMO, START_DT, END_DT) "
-				+ " VALUES(?, ?, ?, ?, ?)";
-
-		int result = 0;
-
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			Connection connection = DriverManager.getConnection(url, user, password);
-
-			PreparedStatement statement = connection.prepareStatement(sql);
-			statement.setString(1, schedule.getScheduleId());
-			statement.setString(2, schedule.getTitle());
-			statement.setString(3, schedule.getMemo());
-			statement.setString(4, schedule.getStartDt());
-			statement.setString(5, schedule.getEndDt());
-
-			result = statement.executeUpdate();
-
-			connection.close();
-		}
-		catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-
-		return result;
+		return scheduleDAO.insertSchedule(schedule);
 	}
 
 	public int modifySchedule(ScheduleVO schedule) {
