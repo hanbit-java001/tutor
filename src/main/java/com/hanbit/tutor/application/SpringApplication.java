a7de7f297fae9f0c5b301919db1d99671cb5520c
hb <hb@hb5000>
2016-09-13 17:00:28 +0900

    스프링 설정파일 생성

--- src/main/java/com/hanbit/tutor/application/SpringApplication.java
+++ src/main/java/com/hanbit/tutor/application/SpringApplication.java
@@ -2,6 +2,8 @@ package com.hanbit.tutor.application;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.Log4jConfigurer;
 
 public class SpringApplication {
@@ -10,9 +12,8 @@ public class SpringApplication {
 		try {
 			Log4jConfigurer.initLogging("classpath:config/log4j.xml");
 
-			Logger logger = LoggerFactory.getLogger(SpringApplication.class);
-
-			logger.debug("hello");
+			ApplicationContext applicationContext =
+					new ClassPathXmlApplicationContext("spring/applicationContext-core.xml");
 		}
 		catch (Exception e) {
 			e.printStackTrace();
