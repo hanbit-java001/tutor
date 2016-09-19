c9daf7ddc8a7577a5a1cf6c800f142dcdc12f240
hb <hb@hb5000>
2016-09-19 16:44:40 +0900

    jackson 추가

--- src/main/java/com/hanbit/tutor/web/controller/WelcomeController.java
+++ src/main/java/com/hanbit/tutor/web/controller/WelcomeController.java
@@ -1,8 +1,12 @@
 package com.hanbit.tutor.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
@@ -13,6 +17,17 @@ public class WelcomeController {
 		model.addAttribute("name", "Hanbit");
 
 		return "welcome";
+	}
+
+	@RequestMapping("/api/data")
+	@ResponseBody
+	public Map getData() {
+		Map map = new HashMap();
+
+		map.put("name", "Hanbit");
+		map.put("message", "Hello");
+
+		return map;
 	}
 
 }
