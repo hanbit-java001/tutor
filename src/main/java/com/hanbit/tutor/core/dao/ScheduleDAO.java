a05d093cc236b0dd7fe2602e532555c8537ebd9f
hb <hb@hb5000>
2016-09-19 12:38:47 +0900

    myBatis 적용

--- src/main/java/com/hanbit/tutor/core/dao/ScheduleDAO.java
+++ src/main/java/com/hanbit/tutor/core/dao/ScheduleDAO.java
@@ -1,10 +1,8 @@
 package com.hanbit.tutor.core.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
@@ -15,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import com.hanbit.tutor.core.vo.ScheduleVO;
 
 @Repository
-public class ScheduleDAO extends AbstarctDAO {
+public class ScheduleDAO {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleDAO.class);
 
@@ -37,104 +35,23 @@ public class ScheduleDAO extends AbstarctDAO {
 	}
 
 	public int deleteSchedule(String scheduleId) {
-		Connection connection = getConnection();
-
-		String sql = "DELETE FROM SCHEDULE WHERE SCHEDULE_ID = ?";
-
-		List params = new ArrayList();
-		params.add(scheduleId);
-
-		int result = executeSql(connection, sql, params);
-
-		closeConnection(connection);
+		int result = sqlSession.delete("schedule.deleteSchedule", scheduleId);
 
 		return result;
 	}
 
 	public List<ScheduleVO> selectSchedules(String startDt, String endDt) {
-		Connection connection = getConnection();
+		Map params = new HashMap();
+		params.put("startDt", startDt);
+		params.put("endDt", endDt);
 
-		String sql = "SELECT SCHEDULE_ID, TITLE, MEMO, "
-				+ "START_DT, END_DT FROM SCHEDULE "
-				+ "WHERE START_DT <= ? AND END_DT >= ? "
-				+ "ORDER BY START_DT";
-
-		List params = new ArrayList();
-		params.add(endDt);
-		params.add(startDt);
-
-		List<ScheduleVO> result = new ArrayList<ScheduleVO>();
-
-		try {
-			PreparedStatement statement = connection.prepareStatement(sql);
-
-			for (int i=0;i<params.size();i++) {
-				statement.setObject(i + 1, params.get(i));
-			}
-
-			ResultSet resultSet = statement.executeQuery();
-
-			while (resultSet.next()) {
-				ScheduleVO schedule = new ScheduleVO();
-
-				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
-				schedule.setTitle(resultSet.getString("TITLE"));
-				schedule.setMemo(resultSet.getString("MEMO"));
-				schedule.setStartDt(resultSet.getString("START_DT"));
-				schedule.setEndDt(resultSet.getString("END_DT"));
-
-				result.add(schedule);
-			}
-
-			resultSet.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		closeConnection(connection);
+		List<ScheduleVO> result = sqlSession.selectList("schedule.selectSchedules", params);
 
 		return result;
 	}
 
 	public ScheduleVO selectSchedule(String scheduleId) {
-		Connection connection = getConnection();
-
-		String sql = "SELECT SCHEDULE_ID, TITLE, MEMO, "
-				+ "START_DT, END_DT FROM SCHEDULE "
-				+ "WHERE SCHEDULE_ID = ?";
-
-		List params = new ArrayList();
-		params.add(scheduleId);
-
-		ScheduleVO schedule = null;
-
-		try {
-			PreparedStatement statement = connection.prepareStatement(sql);
-
-			for (int i=0;i<params.size();i++) {
-				statement.setObject(i + 1, params.get(i));
-			}
-
-			ResultSet resultSet = statement.executeQuery();
-
-			if (resultSet.next()) {
-				schedule = new ScheduleVO();
-
-				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
-				schedule.setTitle(resultSet.getString("TITLE"));
-				schedule.setMemo(resultSet.getString("MEMO"));
-				schedule.setStartDt(resultSet.getString("START_DT"));
-				schedule.setEndDt(resultSet.getString("END_DT"));
-			}
-
-			resultSet.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		closeConnection(connection);
+		ScheduleVO schedule = sqlSession.selectOne("schedule.selectSchedule", scheduleId);
 
 		return schedule;
 	}
