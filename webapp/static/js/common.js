ac5f9effcb9a0a1facf396af42ad944ac6cf9821
hb <hb@hb5000>
2016-10-11 15:11:55 +0900

    common 함수 변경

--- webapp/static/js/common.js
+++ webapp/static/js/common.js
@@ -1,10 +1,5 @@
 function callAjax(ajaxObj) {
-	$.ajax({
-		url: ajaxObj.url,
-		method: ajaxObj.method,
-		data: ajaxObj.data
-	}).done(ajaxObj.success)
-	.fail(function(jqXHR, textStatus, errorThrown) {
+	$.ajax(ajaxObj).fail(function(jqXHR, textStatus, errorThrown) {
 		var errorMsg = "잠시 후 사용해 주세요.";
 
 		if (jqXHR.status == 1500) {
